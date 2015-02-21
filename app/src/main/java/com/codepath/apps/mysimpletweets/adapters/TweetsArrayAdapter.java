de9a5531d383036444ec826251cc500820d0e117
Pritesh Shah <pritesh.shah@magnet.com>
2015-02-20 17:17:34 -0800

    Added timestamp, retweet count and favorite count

--- app/src/main/java/com/codepath/apps/mysimpletweets/adapters/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/adapters/TweetsArrayAdapter.java
@@ -1,6 +1,8 @@
 package com.codepath.apps.mysimpletweets.adapters;
 
 import android.content.Context;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +14,17 @@ import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
+
+    private final String TWITTER_DATE_FORMAT = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
 
     public TweetsArrayAdapter(Context context, List<Tweet> tweets) {
         super(context, R.layout.item_tweet, tweets);
@@ -29,6 +36,9 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
         @InjectView(R.id.tvName) TextView tvName;
         @InjectView(R.id.tvUserName) TextView tvUserName;
         @InjectView(R.id.tvBody) TextView tvBody;
+        @InjectView(R.id.tvRetweetCount) TextView tvRetweetCount;
+        @InjectView(R.id.tvFavoriteCount) TextView tvFavoriteCount;
+        @InjectView(R.id.tvTimestamp) TextView tvTimestamp;
 
         public ViewHolder(View view) {
             ButterKnife.inject(this, view);
@@ -54,6 +64,18 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
         viewHolder.tvUserName.setText("@" + tweet.getUser().getScreenName());
         viewHolder.tvBody.setText(tweet.getBody());
         viewHolder.ivProfileImage.setImageResource(android.R.color.transparent);
+        viewHolder.tvRetweetCount.setText(String.valueOf(tweet.getRetweetCount()));
+        viewHolder.tvFavoriteCount.setText(String.valueOf(tweet.getFavoritesCount()));
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(TWITTER_DATE_FORMAT);
+            simpleDateFormat.setLenient(true);
+            Date date = simpleDateFormat.parse(tweet.getCreatedAt());
+            String dateString = (String) DateUtils.getRelativeTimeSpanString(date.getTime(), System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
+            dateString = formatDateString(dateString);
+            viewHolder.tvTimestamp.setText(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         Picasso.with(getContext())
                 .load(tweet.getUser().getProfileImageUrl())
@@ -61,5 +83,18 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
 
         // Return the completed view to render on screen
         return convertView;
+    }
+
+    // Dirty hack! How do we deal with locales?
+    private String formatDateString(String dateString) {
+        dateString = dateString.replace(" ago", "");
+        dateString = dateString.replace(" seconds", "s");
+        dateString = dateString.replace(" second", "s");
+        dateString = dateString.replace(" minutes", "m");
+        dateString = dateString.replace(" minute", "m");
+        dateString = dateString.replace(" hours", "h");
+        dateString = dateString.replace(" hour", "h");
+
+        return dateString;
     }
 }
