e8a4cd4f0e5c9265ae1b94121acb55a95b525029
Pritesh Shah <pritesh.shah@magnet.com>
2015-02-22 10:36:05 -0800

    Added tweet button placeholder

--- app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
@@ -115,16 +115,13 @@ public class TimelineActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.miTweet:
+//                composeTweet();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
