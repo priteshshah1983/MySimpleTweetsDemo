5e4d0c96b4e0231eba23f13f43d29c805c0e07af
Pritesh Shah <pritesh.shah@magnet.com>
2015-03-01 19:23:58 -0800

    Issue explained here: http://stackoverflow.com/questions/21295991/can-not-perform-this-action-after-onsaveinstancestate-dialogfragment

--- app/src/main/java/com/codepath/apps/mysimpletweets/fragments/TweetFragment.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/fragments/TweetFragment.java
@@ -1,7 +1,7 @@
 package com.codepath.apps.mysimpletweets.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.app.DialogFragment;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
